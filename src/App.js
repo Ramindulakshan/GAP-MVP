7f566362df6f3685dda90bd65398c19ad73d4eea
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2024-01-11 01:30:30 +0530

    set routes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,29 @@
-import React from 'react';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-// import Navbar from './components/Navbar/Navbar';
-// import HomePage from './components/HomePage/HomePage';
-// import Footer from './components/Footer/Footer';
-import LoginPage from './components/LoginPage/LoginPage';
-import LandingPage from './components//LandingPage/Landing';
-import ResetPassword from './components/FogotPassword/ResetPassword';
-import EmailVerify from './components/EmailVerify/EmailVerify';
-import MobileAuthentication from './components/MobileAuthentication/MobileAuthentication';
-import FogotPassword from './components/FogotPassword/FogotPassword';
-import VerifyMN from './components/FogotPassword/VerifyMN';
-import VerifyE from './components/FogotPassword/VerifyE';
-import VerificationPage from './components/Verification/VerificationPage';
-// import LoginPageS from './components/Student/LoginPageS';
-import HomePage from './components/HomePage/HomePage';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import LoginPage from "./components/LoginPage/LoginPage";
+import LandingPage from "./components/LandingPage/Landing";
 
-const App=()=> {
-return (
+const App = () => {
+  return (
+    //  <LoginPage />
+    //  <FogotPassword />
+    //  <VerifyMN />
+    //  <VerifyE />
+    //  <ResetPassword />
+    // <MobileAuthentication />
+    //  <VerificationPage />
+    // <HomePage />
+    // <LandingPage />
 
-<>
-{/* <LoginPage/> */}
-{/* <FogotPassword/> */}
-{/* <VerifyMN/> */}
-{/* <VerifyE/> */}
-{/* <ResetPassword/> */}
-{/* <MobileAuthentication/> */}
-{/* <VerificationPage/> */}
-{/* <HomePage/> */}
-<LandingPage/>
-
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   );
-}                 
+};
 
 export default App;
