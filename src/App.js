fbf9970d0d75064b624e9be2605d7dfff68f5e15
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2024-01-12 23:31:51 +0530

    set routes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,32 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-// import Navbar from './components/Navbar/Navbar';
-// import HomePage from './components/HomePage/HomePage';
-// import Footer from './components/Footer/Footer';
 import LoginPage from "./components/LoginPage/LoginPage";
-import LandingPage from "./components//LandingPage/Landing";
-import ResetPassword from "./components/FogotPassword/ResetPassword";
-import EmailVerify from "./components/EmailVerify/EmailVerify";
-import MobileAuthentication from "./components/MobileAuthentication/MobileAuthentication";
-import FogotPassword from "./components/FogotPassword/FogotPassword";
-import VerifyMN from "./components/FogotPassword/VerifyMN";
-import VerifyE from "./components/FogotPassword/VerifyE";
-import VerificationPage from "./components/Verification/VerificationPage";
-// import LoginPageS from './components/Student/LoginPageS';
+import LandingPage from "./components/LandingPage/Landing";
+import ProLogin from "./components/ProLogin/Prologin";
 import HomePage from "./components/HomePage/HomePage";
-import MentoringSession from"./components/MentoringSession/MentoringSession";
-import WeeklySchedulePageNotMenter from "./components/WeeklySchedulePageNotMenter/WeeklySchedulePageNotMenter";
-import WeeklySchedulePage from "./components/WeeklySchedulePage/WeeklySchedulePage";
+
 const App = () => {
   return (
-    <>
-      {/* <LoginPage/> */}
-      {/* <FogotPassword/> */}
-      {/* <VerifyMN/> */}
-      {/* <VerifyE/> */}
-      {/* <ResetPassword/> */}
-      {/* <MobileAuthentication/> */}
-      {/* <VerificationPage/> */}
-      {/* <HomePage/> */}
-      {/*<LandingPage/>*/}
-      {/*<SideBar/>*/}
-      {/* <WeeklySchedulePageNotMenter/>*/}
-      {/**/}
-      {/**/}
-      {/**/}
-      {/**/}
-     
-      <WeeklySchedulePage/>
-    </>
+    //  <LoginPage />
+    //  <FogotPassword />
+    //  <VerifyMN />
+    //  <VerifyE />
+    //  <ResetPassword />
+    // <MobileAuthentication />
+    //  <VerificationPage />
+    // <HomePage />
+    // <LandingPage />
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<ProLogin />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
