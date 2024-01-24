a253942447893661a2b083f1349a4141758a779c
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2024-01-24 22:06:27 +0530

    setting route

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-// import Navbar from './components/Navbar/Navbar';
-// import HomePage from './components/HomePage/HomePage';
-// import Footer from './components/Footer/Footer';
 import LoginPage from "./components/LoginPage/LoginPage";
 import LandingPage from "./components//LandingPage/Landing";
-import ResetPassword from "./components/FogotPassword/ResetPassword";
-import EmailVerify from "./components/EmailVerify/EmailVerify";
-import MobileAuthentication from "./components/MobileAuthentication/MobileAuthentication";
-import FogotPassword from "./components/FogotPassword/FogotPassword";
-import VerifyMN from "./components/FogotPassword/VerifyMN";
-import VerifyE from "./components/FogotPassword/VerifyE";
-import VerificationPage from "./components/Verification/VerificationPage";
-// import LoginPageS from './components/Student/LoginPageS';
 import HomePage from "./components/HomePage/HomePage";
-import BeAMentor from "./components/BeAMentor/BeAMentor";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import ProLogin from "./components/Register/Register";
-import Example from "./components/UserProfile/UserProfile";
+import Settings from "./components/Settings/Settings";
 
 const App = () => {
   return (
     //  <LoginPage />
-    //  <FogotPassword />
+    //  <ForgotPassword />
     //  <VerifyMN />
     //  <VerifyE />
     //  <ResetPassword />
@@ -31,19 +19,18 @@ const App = () => {
     //  <VerificationPage />
     // <HomePage />
     //
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<LandingPage />} />
-    //     <Route path="/login" element={<LoginPage />} />
-    //     <Route path="/register" element={<ProLogin />} />
-    //     <Route path="/home" element={<HomePage />} />
-    //   </Routes>
-    // </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<ProLogin />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </BrowserRouter>
 
-    <div>
-      <Example />
-    </div>
   );
 };
 
 export default App;
+
