e5e72c6dbeb34d79171e0a1a225cafa706a39617
Shamindi <ngdilshan2002@gmail.com>
2024-01-31 13:36:07 +0530

    DONE

--- src/App.js
+++ src/App.js
@@ -1,48 +1,84 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import LoginPage from "./components/LoginPage/LoginPage";
-import LandingPage from "./components//LandingPage/Landing";
-import HomePage from "./components/HomePage/HomePage";
-import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import ProLogin from "./components/Register/Register";
-import SettingPage from "./components/SettingPage/SettingPage";
-import WeeklySchedulePage from "./components/WeeklySchedulePage/WeeklySchedulePage";
-import UserProfile from "./components/UserProfile/UserProfile";
+
+//Pages Import Section
+import BeAMentor from "./components/BeAMentor/BeAMentor";
+import UnderConstructionBeAMentor from "./components/BeAMentor/UnderConstructionBeAMentor";
+import EmailVerify from "./components/EmailVerify/EmailVerify";
 import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
-import VerifyE from "./components/ForgotPassword/VerifyE";
 import ResetPassword from "./components/ForgotPassword/ResetPassword";
-import MentoringSessionPage from "./components/MentoringSession/MentoringSessionEmpty";
-import History from './components/HistoryPage/UnderConstructionHistoryPage';
+import VerifyE from "./components/ForgotPassword/VerifyE";
+import VerifyMN from "./components/ForgotPassword/VerifyMN";
+import HistoryPage from "./components/HistoryPage/HistoryPage";
+import HistoryPageEmpty from "./components/HistoryPage/HistoryPageEmpty";
+import UnderConstructionHistoryPage from "./components/HistoryPage/UnderConstructionHistoryPage";
+import HomePage from "./components/HomePage/HomePage";
+import UnderConstructionHome from "./components/HomePage/UnderConstructionHome";
+import LandingPage from "./components//LandingPage/Landing";
+import LandingPageAbout from "./components/LandingPageAbout/LandingPageAbout";
+import LandingPageContact from "./components/LandingPageContact/LandingPageContact";
+import LoginPage from "./components/LoginPage/LoginPage";
+import MentoringSessionPage from "./components/MentoringSession/MentoringSession";
+import MentoringSessionEmpty from "./components/MentoringSession/MentoringSessionEmpty";
+import UnderConstructionMentorSession from "./components/MentoringSession/UnderConstructionMentorSession";
+import MentorsFilter from "./components/MentorsFilter/Filters";
+import Mentors from "./components/MentorsPage/Mentors"
+import UnderConstructionMentors from "./components/MentorsPage/UnderConstructionMentors";
+import MobileAuthentication from "./components/MobileAuthentication/MobileAuthentication";
+import Notification from "./components/Notification/Notification";
+import Register from "./components/Register/Register";
+import Request from "./components/Request/Request";
+import SettingPage from "./components/SettingPage/SettingPage";
+import UserProfile from "./components/UserProfile/UserProfile";
+import UserProfileEmptyView from "./components/UserProfile/UserProfileEmptyView";
+import UserProfileOtherUsers from "./components/UserProfile/UserProfileOtherUsers";
+import VerificationPage from "./components/Verification/VerificationPage";
+import WeeklySchedulePage from "./components/WeeklySchedulePage/WeeklySchedulePage";
+import UnderConstructionWeeklySchedulePage from "./components/WeeklySchedulePage/UnderConstructionWeeklySchedulePage";
+import WeeklySchedulePageNotMenter from "./components/WeeklySchedulePageNotMenter/WeeklySchedulePageNotMenter"
+
 const App = () => {
   return (
-    //  <LoginPage />
-    //  <ForgotPassword />
-    //  <VerifyMN />
-    //  <VerifyE />
-    //  <ResetPassword />
-    // <MobileAuthentication />
-    //  <VerificationPage />
-    // <HomePage />
-    //
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<LandingPage />} />
-    //     <Route path="/login" element={<LoginPage />} />
-    //     <Route path="/register" element={<ProLogin />} />
-    //     <Route path="/home" element={<HomePage />} />
-    //     <Route path="/settings" element={<SettingPage />} />
-    //     <Route path="/WeeklySchedulePage" element={<WeeklySchedulePage />} />
-    //     <Route path="/userProfile" element={<UserProfile />} />
-    //     <Route path="/forgotPassword" element={<ForgotPassword />} />
-    //     <Route path="/verifyE" element={<VerifyE />} />
-    //     <Route path="/resetPassword" element={<ResetPassword />} />
-    //     <Route path="/mentoringSession " element={<MentoringSessionPage  />} />
-    //   </Routes>
-    // </BrowserRouter>
-    <div>
-      <History />
-    </div>
+    <React.Fragment>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/settings" element={<SettingPage />} />
+        <Route path="/WeeklySchedulePage" element={<WeeklySchedulePage />} />
+        <Route path="/userProfile" element={<UserProfile />} />
+        <Route path="/forgotPassword" element={<ForgotPassword />} />
+        <Route path="/verifyE" element={<VerifyE />} />
+        <Route path="/resetPassword" element={<ResetPassword />} />
+        <Route path="/mentoringSession " element={<MentoringSessionPage  />} />
+        <Route path="/beAMentor " element={<BeAMentor  />} />
+        <Route path="/underConstructionBeAMentor " element={<UnderConstructionBeAMentor  />} />
+        <Route path="/emailVerify " element={<EmailVerify  />} />
+        <Route path="/verifyMN " element={<VerifyMN  />} />
+        <Route path="/historyPage " element={<HistoryPage  />} />
+        <Route path="/historyPageEmpty " element={<HistoryPageEmpty  />} />
+        <Route path="/underConstructionHistoryPage " element={<UnderConstructionHistoryPage  />} />
+        <Route path="/underConstructionHome " element={<UnderConstructionHome  />} />
+        <Route path="/landingPageAbout " element={<LandingPageAbout  />} />
+        <Route path="/landingPageContact " element={<LandingPageContact  />} />
+        <Route path="/mentoringSessionEmpty " element={<MentoringSessionEmpty  />} />
+        <Route path="/underConstructionMentorSession " element={<UnderConstructionMentorSession  />} />
+        <Route path="/mentors " element={<Mentors  />} />
+        <Route path="/mentorsFilter " element={<MentorsFilter  />} />
+        <Route path="/underConstructionMentors " element={<UnderConstructionMentors  />} />
+        <Route path="/mobileAuthentication " element={<MobileAuthentication  />} />
+        <Route path="/notification " element={<Notification  />} />
+        <Route path="/request " element={<Request  />} />  
+        <Route path="/userProfileEmptyView " element={<UserProfileEmptyView  />} />
+        <Route path="/userProfileOtherUsers " element={<UserProfileOtherUsers  />} />
+        <Route path="/verificationPage " element={<VerificationPage  />} />
+        <Route path="/underConstructionWeeklySchedulePage " element={<UnderConstructionWeeklySchedulePage  />} />
+        <Route path="/weeklySchedulePageNotMenter " element={<WeeklySchedulePageNotMenter  />} />
+      </Routes>
+    </React.Fragment>
   );
 };
 
