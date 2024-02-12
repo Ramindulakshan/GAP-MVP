e371621abc95de3394b042eafef0ece35b790e9a
Ramindu Jayaweera <ramindujayaweera2000@gmail.com>
2024-02-12 18:00:41 +0530

    new

--- src/components/LandingPageAbout/LandingPageAbout.js
+++ src/components/LandingPageAbout/LandingPageAbout.js
@@ -1,67 +1,53 @@
 import React from "react";
 import "../LandingPage/Landing.css";
 import GAP_Image from "../HomePage/Img/GAP_BG.png";
-import pro1 from "./img/Ellipse 62 (1).png";
-import pro2 from "./img/Ellipse 63.png";
-import pro3 from "./img/Ellipse 64.png";
-import pro4 from "./img/Ellipse 65.png";
-import pro5 from "./img/Ellipse 66.png";
+import pro1 from "./img/KanishkaWeeramunda.jpg";
+import pro2 from "./img/MiuliKalubovila.jpg";
+import pro3 from "./img/ChathushkeeAmuwatta.jpeg";
+import pro4 from "./img/IsuruUdana.jpeg";
+import pro5 from "./img/TharushiJayawardana.jpg";
+import pro6 from "./img/Anjana.jpg";
+import pro7 from "./img/RaminduLakshan.jpg";
+import pro8 from "./img/DilshanPriyawansha.jpg";
+import pro9 from "./img/YasinduSanjeewa.jpg";
+import pro10 from "./img/GaurawaBandara.jpg";
+import pro11 from "./img/UshmikaKavishan.jpg";
 function LandingPageAbout() {
   return (
     <div>
       <section className="">
         <div className="vh-100">
-          <div className="container">
+        <div className="container">
             <nav className="navbar navbar-light">
-              <a className="navbar-brand" href="#c">
+              <a className="navbar-brand" href="/">
                 <img src={GAP_Image} height="90" alt="" />
               </a>
-              <div className="d-flex justify-content-end">
-                <a
-                  className="nav-link custom-nav-link "
-                  href="home"
-                  onClick={() => {
-                    window.location.href = "/underConstructionHome";
-                  }}
-                >
-                  Home
-                </a>
-                <a
-                  className="nav-link custom-nav-link "
-                  href="landingPageAbout"
-                  onClick={() => {
-                    window.location.href = "/landingPageAbout";
-                  }}
-                >
-                  About
-                </a>
-                <a
-                  className="nav-link custom-nav-link"
-                  href="landingPageContact"
-                  onClick={() => {
-                    window.location.href = "/landingPageContact";
-                  }}
-                >
-                  Contact
-                </a>
-                <a
-                  className="nav-link custom-nav-link"
-                  href="login"
-                  onClick={() => {
-                    window.location.href = "/login";
-                  }}
-                >
-                  Login
-                </a>
-                <form className="form-inline my-2 my-lg-0">
-                  <button
-                    className="btn custom-button1 my-2 my-sm-3"
-                    type="submit"
-                  >
-                    Register
-                  </button>
-                </form>
-              </div>
+              <nav class=" navbar-expand-lg navbar-light bg-light">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto">
+                    <li class="">
+                      <a class="nav-link custom-nav-link  " href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link custom-nav-link " href="/landingPageAbout">About</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link custom-nav-link " href="/landingPageContact">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link custom-nav-link " href="/login">Login</a>
+                    </li>
+                  </ul>
+                  <form class="form-inline my-2 my-lg-0 mt-4">
+                    <button class="btn custom-button1 my-2 my-sm-3" type="button" onclick="window.location.href='/register'">Register</button>
+                  </form>
+                </div>
+              </nav>
+
             </nav>
           </div>
           <div className="text-center cenclz mt-4 ">
@@ -79,29 +65,35 @@ function LandingPageAbout() {
             </p>
           </div>
           <div className="dtabx">
-            <h5 className="card-title fw-bold titlabt2">Meet the team </h5>
+            <h5 className="card-title fw-bold titlabt2 mb-4">Meet the team </h5>
             <div className="cardsdn">
-              <div className="container mt-2">
+              <div className="container">
                 <div className="row">
-                  <div className="col-sm-4 col-md-4 col-lg-2 text-center">
+                  <div className="col-sm-2 col-md-2 col-lg-2 text-center">
                     <div className="pbtn2oo">
                       <img
                         src={pro1}
                         alt="logo"
-                        className="img-fluid custom-image-Gap"
+                        className="img-fluid custom-image-Gap rounded-circle"
+                        style={{ width: '100px', height: '100px', objectFit: 'cover' }}
                       />
                     </div>
-                    <p className="mb-0 pintflnme">Full Name</p>
+                    <p className="mb-0 pintflnme">Kanishka Weeramunda</p>
+                    <p className="mb-0 ">(Director/Advisor)
+</p>
                   </div>
+
                   <div className="col-sm-4 col-md-4 col-lg-2 text-center">
                     <div className="pbtn2oo">
                       <img
                         src={pro2}
                         alt="logo"
-                        className="img-fluid custom-image-Gap"
+                        className="img-fluid custom-image-Gap rounded-circle"
+                        style={{ width: '100px', height: '100px', objectFit: 'cover' }}
                       />
                     </div>
-                    <p className="mb-0 pintflnme">Full Name</p>
+                    <p className="mb-0 pintflnme">Miuli Kalubovila</p>
+                    <p className="mb-0 ">(Founder)</p>
                   </div>
 
                   <div className="col-sm-4 col-md-4 col-lg-2 text-center">
@@ -109,35 +101,132 @@ function LandingPageAbout() {
                       <img
                         src={pro3}
                         alt="logo"
-                        className="img-fluid custom-image-Gap"
+                        className="img-fluid custom-image-Gap rounded-circle"
+                        style={{ width: '100px', height: '100px', objectFit: 'cover' }}
                       />
                     </div>
-                    <p className="mb-0 pintflnme">Full Name</p>
+                    <p className="mb-0 pintflnme">Chathushkee Amuwatta</p>
+                    <p className="mb-0 ">(Senior Project Manager)</p>
                   </div>
                   <div className="col-sm-4 col-md-4 col-lg-2 text-center">
                     <div className="pbtn2oo">
                       <img
                         src={pro4}
                         alt="logo"
-                        className="img-fluid custom-image-Gap"
+                        className="img-fluid custom-image-Gap rounded-circle"
+                        style={{ width: '100px', height: '100px', objectFit: 'cover' }}
                       />
                     </div>
-                    <p className="mb-0 pintflnme">Full Name</p>
+                    <p className="mb-0 pintflnme">Isuru Udana</p>
+                    <p className="mb-0 ">(Project Manager)
+</p>
                   </div>
                   <div className="col-sm-4 col-md-4 col-lg-2 text-center">
                     <div className="pbtn2oo">
                       <img
                         src={pro5}
                         alt="logo"
-                        className="img-fluid custom-image-Gap"
+                        className="img-fluid custom-image-Gap rounded-circle"
+                        style={{ width: '100px', height: '100px', objectFit: 'cover' }}
                       />
                     </div>
-                    <p className="mb-0 pintflnme">Full Name</p>
+                    <p className="mb-0 pintflnme">Tharushi Jayawardana</p>
+                    <p className="mb-0 ">(UI/UX Designer)
+</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="dtabx">
+            <div className="">
+              <div className="container">
+                <div className="row">
+                  <div className="col-sm-4 col-md-4 col-lg-2 text-center">
+                    <div className="pbtn2oo">
+                      <img
+                        src={pro6}
+                        alt="logo"
+                        className="img-fluid custom-image-Gap rounded-circle"
+                        style={{ width: '100px', height: '100px', objectFit: 'cover' }}
+                      />
+                    </div>
+                    <p className="mb-0 pintflnme">Irani Aanjana</p>
+                    <p className="">(UI/UX Designer)</p>
+                  </div>
+                  <div className="col-sm-4 col-md-4 col-lg-2 text-center">
+                    <div className="pbtn2oo">
+                      <img
+                        src={pro7}
+                        alt="logo"
+                        className="img-fluid custom-image-Gap rounded-circle"
+                        style={{ width: '100px', height: '100px', objectFit: 'cover' }}
+                      />
+                    </div>
+                    <p className="mb-0 pintflnme">Ramindu Lakshan</p>
+                    <p className="mb-0 ">(Frontend Developer)
+</p>
+                  </div>
+
+
+                  <div className="col-sm-4 col-md-4 col-lg-2 text-center">
+                    <div className="pbtn2oo">
+                      <img
+                        src={pro8}
+                        alt="logo"
+                        className="img-fluid custom-image-Gap rounded-circle"
+                        style={{ width: '100px', height: '100px', objectFit: 'cover' }}
+                      />
+                    </div>
+                    <p className="mb-0 pintflnme">Dilshan Priyawansha</p>
+                    <p className="mb-0 ">(Frontend Developer)
+</p>
+                  </div>
+                  <div className="col-sm-4 col-md-4 col-lg-2 text-center">
+                    <div className="pbtn2oo">
+                      <img
+                        src={pro9}
+                        alt="logo"
+                        className="img-fluid custom-image-Gap rounded-circle"
+                        style={{ width: '100px', height: '100px', objectFit: 'cover' }}
+                      />
+                    </div>
+                    <p className="mb-0 pintflnme">Yasindu Sanjeewa</p>
+                    <p className="mb-0 ">(Backend Developer)
+</p>
+                  </div>
+                  <div className="col-sm-4 col-md-4 col-lg-2 text-center">
+                    <div className="pbtn2oo">
+                      <img
+                        src={pro10}
+                        alt="logo"
+                        className="img-fluid custom-image-Gap rounded-circle"
+                        style={{ width: '100px', height: '100px', objectFit: 'cover' }}
+                      />
+                    </div>
+                    <p className="mb-0 pintflnme">Gaurawa Bandara</p>
+                    <p className="mb-0 ">(Backend Developer)
+</p>
+                  </div>
+                  <div className="col-sm-4 col-md-4 col-lg-2 text-center">
+                    <div className="pbtn2oo">
+                      <img
+                        src={pro11}
+                        alt="logo"
+                        className="img-fluid custom-image-Gap rounded-circle"
+                        style={{ width: '100px', height: '100px', objectFit: 'cover' }}
+                      />
+                    </div>
+                    <p className="mb-0 pintflnme">Ushmika Kavishan</p>
+                    <p className="mb-0 ">(Quality Assurance)</p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+
         </div>
       </section>
     </div>
