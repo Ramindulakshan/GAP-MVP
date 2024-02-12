ad0f0d30c179d6e908257226297978ef1308f158
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2024-02-13 00:29:04 +0530

    format document

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -17,11 +17,9 @@ const LoginPage = () => {
 
   const [showPassword, setShowPassword] = useState(false);
 
-const togglePasswordVisibility = () => {
+  const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
-
-
 
   const handleSubmit = (e) => {
     const form = e.currentTarget;
@@ -45,9 +43,9 @@ const togglePasswordVisibility = () => {
           getUserName();
           alert("Login Successful");
           navigate("/home");
-        } else if(response.data.status === "error") {
+        }
+        if (response.data.status === "error") {
           alert("Invalid Username or Password");
-          console.log("Invalid Username or Password");
         }
       })
       .catch((error) => {
@@ -78,128 +76,121 @@ const togglePasswordVisibility = () => {
   //     "http://localhost:3001/auth/google/callback",
   //     "_self"
   //   );
-  // } //google login 
- 
+  // } //google login
+
   return (
     <>
       <section className="newwitchg">
-       
         <div className="container-fluid container-fluidreg">
-        <div className="">
-        <br></br> <br></br> <br></br>
-          <div className="row bxbxsetlog">
-            <div className="col-lg-5  custom-bglog  align-items-center justify-content-center">
-              <div className=" text-center">
-                <img
-                  src={Register_image}
-                  alt="Log Img"
-                  className="img-fluid custom-image-LI"
-                  style={{ maxHeight: "100%", width: "100%" }}
-                />
+          <div className="">
+            <br></br> <br></br> <br></br>
+            <div className="row bxbxsetlog">
+              <div className="col-lg-5  custom-bglog  align-items-center justify-content-center">
+                <div className=" text-center">
+                  <img
+                    src={Register_image}
+                    alt="Log Img"
+                    className="img-fluid custom-image-LI"
+                    style={{ maxHeight: "100%", width: "100%" }}
+                  />
+                </div>
               </div>
-            </div>
-            <div className="col-lg-6 overflow-auto "><br />
-              <h1 className="text-center mb-5 mt-5 ">Login to Account</h1>
-              <div className="row justify-content-center">
-                <div className="col-lg-9 sitewyey-log">
-                  <div className="form-outline">
-                    <br /> 
+              <div className="col-lg-6 overflow-auto ">
+                <br />
+                <h1 className="text-center mb-5 mt-5 ">Login to Account</h1>
+                <div className="row justify-content-center">
+                  <div className="col-lg-9 sitewyey-log">
+                    <div className="form-outline">
+                      <br />
 
-                    <Form
-                      noValidate
-                      validated={validated}
+                      <Form noValidate validated={validated}>
+                        <Form.Group controlId="validationCustomUsername">
+                          <InputGroup hasValidation>
+                            <Form.Control
+                              type="text"
+                              placeholder="Username"
+                              style={{ textTransform: "none" }}
+                              aria-describedby="inputGroupPrepend"
+                              required
+                              onChange={(e) => setUserName(e.target.value)}
+                            />
+                            <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+                          </InputGroup>
+                        </Form.Group>
+                        <br></br>
+
+                        <Form.Group controlId="validationCustomUsername">
+                          <InputGroup hasValidation>
+                            <Form.Control
+                              type={showPassword ? "text" : "password"}
+                              placeholder="Password"
+                              value={password}
+                              aria-describedby="passwordHelpBlock2"
+                              id="inputConfirmPassword"
+                              style={{ textTransform: "none" }}
+                              required
+                              onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <InputGroup.Text
+                              id="passwordHelpBlock"
+                              onClick={togglePasswordVisibility}
+                            >
+                              {!showPassword ? (
+                                <HiOutlineEyeOff />
+                              ) : (
+                                <HiOutlineEye />
+                              )}
+                            </InputGroup.Text>
+                            <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+                          </InputGroup>
+                        </Form.Group>
+                      </Form>
+                    </div>
+                    <h5
+                      className="mb-4 mt-2 custom-text-FP ore "
+                      style={{ cursor: "pointer" }}
+                      onClick={() => {
+                        window.location.href = "/forgotPassword";
+                      }}
                     >
-                      <Form.Group controlId="validationCustomUsername">
-                        <InputGroup hasValidation>
-                          <Form.Control
-                            type="text"
-                            placeholder="Username"
-                            style={{ textTransform: "none" }}
-                            aria-describedby="inputGroupPrepend"
-                            required
-                            onChange={(e) => setUserName(e.target.value)}
-                          />
-                          <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
-                        </InputGroup>
-                      </Form.Group>
-                      <br></br>
-
-                      <Form.Group controlId="validationCustomUsername">
-                        <InputGroup hasValidation>
-                          <Form.Control
-                            
-                            type ={showPassword ? "text" : "password"}
-                            placeholder="Password"
-                            value={password}
-                            aria-describedby="passwordHelpBlock2"
-                            id="inputConfirmPassword"
-
-                            style={{ textTransform: "none" }}
-                            
-                            required
-                            onChange={(e) => setPassword(e.target.value)}
-                          />
-                          <InputGroup.Text
-                            id="passwordHelpBlock"
-                            onClick={togglePasswordVisibility}
-                          >
-                            {!showPassword ? <HiOutlineEyeOff /> : <HiOutlineEye />}
-                          </InputGroup.Text>
-                          <Form.Control.Feedback type="invalid">
-                            
-                          </Form.Control.Feedback>
-                        </InputGroup>
-                      </Form.Group>
-                    </Form>
-                  </div>
-                  <h5
-                    className="mb-4 mt-2 custom-text-FP ore "
-                    style={{ cursor: "pointer" }}
-                    onClick={() => {
-                      window.location.href = "/forgotPassword";
-                    }}
-                  >
-                    Forgot password?
-                  </h5>
-                  <div className="text-center">
-                    <button
-                      className="custom-button2rtyu  my-1 my-sm-3"
-                      type="submit"
-                      onClick={handleSubmit}
-                    >
-                      Login
-                    </button>
-                  </div>
-               {/* {   <div class="text-center mb-4">
+                      Forgot password?
+                    </h5>
+                    <div className="text-center">
+                      <button
+                        className="custom-button2rtyu  my-1 my-sm-3"
+                        type="submit"
+                        onClick={handleSubmit}
+                      >
+                        Login
+                      </button>
+                    </div>
+                    {/* {   <div class="text-center mb-4">
                     <h4 className="mb-4 ore">Or, login with</h4>
                     <div className="Icon">
                       <img src={Google_image} alt="Image1" />
                     </div>
                   </div>} */}
-                  <br /> <br />
-                  <h6 className="custom-text-AR ore">
-                    Don't have an account?{" "}
-                    <span
-                      style={{ cursor: "pointer", color: "blue" }}
-                      onClick={() => {
-                        window.location.href = "/register";
-                      }}
-                    >
-                      Register
-                    </span>
-                  </h6>
+                    <br /> <br />
+                    <h6 className="custom-text-AR ore">
+                      Don't have an account?{" "}
+                      <span
+                        style={{ cursor: "pointer", color: "blue" }}
+                        onClick={() => {
+                          window.location.href = "/register";
+                        }}
+                      >
+                        Register
+                      </span>
+                    </h6>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        </div>
       </section>
-      
     </>
   );
-  
 };
-
 
 export default LoginPage;
