e65576fcf8f1a12a06b51e762576fb0202f1210c
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2024-03-25 22:38:40 +0530

    backendUrl

--- src/backendUrl.js
+++ src/backendUrl.js
@@ -1,5 +1,5 @@
-// export const backEndURL = "https://generationalpha.onrender.com";
+export const backEndURL = "https://api.generationalpha/api";
 // export const backEndURL = "http://localhost:3001/api";
-export const backEndURL = "http://89.116.34.229/api"
+// export const backEndURL = "http://89.116.34.229/api"
 
 
