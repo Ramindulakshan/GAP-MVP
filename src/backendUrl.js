42a7a75cc5618077d610618154069fc1006ee99e
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2024-04-17 15:53:53 +0530

    fixed url

--- src/backendUrl.js
+++ src/backendUrl.js
@@ -1,4 +1,4 @@
-export const backEndURL = "https://api.generationalpha/api";
+export const backEndURL = "https://api.generationalpha.info/api";
 // export const backEndURL = "http://localhost:3001/api";
 // export const backEndURL = "http://89.116.34.229/api"
 
