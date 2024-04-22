3cf10800b67ec7217231d47363c708da465274e7
Dila <99160680+Dila25@users.noreply.github.com>
2024-04-22 17:25:42 +0530

    nav bar fi

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ const App = () => {
   return (
     <React.Fragment>
       <Routes>
-        <Route element={<HandleAuth />}>
+        <Route >
           <Route path="/home" element={<HomePage />} />
           <Route path="/settings" element={<SettingPage />} />
           <Route path="/mentoringSession" element={<MentoringSessionPage />} />
